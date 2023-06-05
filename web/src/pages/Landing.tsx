bb5cd4728c1ed7caa984c58ef17d9b8d70b9772d
Robert McCurdy <rwmccurdy@hotmail.com>
2023-06-04 19:12:46 -0700

    asdf

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { Box } from '@chakra-ui/layout'
-
 export const Landing = () => {
-  return <Box>Landing</Box>
+  return (
+    <div>
+      <p>Ladning</p>
+    </div>
+  )
 }
